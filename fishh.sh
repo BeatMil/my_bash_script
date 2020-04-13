b84f92f48726a3d862289c1a6a1ee1d13f856842
BeatMil <anutorn@gmail.com>
2020-04-14 00:05:14 +0700

    fixed pingb.sh

---
+++ fishh.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+name=beat16
+name=${name:3}
+echo $name
--- pingb.sh
+++ pingb.sh
@@ -4,16 +4,16 @@ while(true)
 do
 	sleep 1
 	data=$(ping -n 1 ${ip} | grep 'Reply' | cut -d ' ' -f 5&)
-	sub_data=$(ping -n 1 ${ip} | grep 'Reply' | cut -d ' ' -f 5| tr -d 'ms'&) # just int
+	sub_data=${data:5:-2} #substring so only int left
 	time=$(date)
-	if(($sub_data>=50))
-	then
-		echo -e "\e[38;5;226m"${data} "\e[0m"${time} >> /d/beatmil/ping_log.txt # yellow
-		echo -e "\e[38;5;226m"${data} "\e[0m"${time} 
-	elif(($sub_data>=90))
+	if(($sub_data>=90))
 	then
 		echo -e "\e[38;5;1m"${data} "\e[0m"${time} >> /d/beatmil/ping_log.txt # red
 		echo -e "\e[38;5;1m"${data} "\e[0m"${time} 
+	elif(($sub_data>=50))
+	then
+		echo -e "\e[38;5;226m"${data} "\e[0m"${time} >> /d/beatmil/ping_log.txt # yellow
+		echo -e "\e[38;5;226m"${data} "\e[0m"${time} 
 	else
 		echo -e ${data} ${time}"\e[0m" >> /d/beatmil/ping_log.txt #text color (always change back to white)
 		echo -e ${data} ${time}"\e[0m" 
